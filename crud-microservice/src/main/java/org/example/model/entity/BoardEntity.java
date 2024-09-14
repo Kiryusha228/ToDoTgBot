1749994a435751ed04dbf4d2d714593832a4674b
Kiryusha228 <125873893+Kiryusha228@users.noreply.github.com>
2024-09-14 13:34:24 +0300

    init

--- crud-microservice/src/main/java/org/example/model/entity/BoardEntity.java
+++ crud-microservice/src/main/java/org/example/model/entity/BoardEntity.java
@@ -1,16 +1,31 @@
 package org.example.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "board")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BoardEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "userchatid")
     private Long userChatId;
 
+    @Column(name = "title")
     private String title;
-}
 
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<TodoEntity> todos;
+}
--- crud-microservice/src/main/java/org/example/model/entity/TodoEntity.java
+++ crud-microservice/src/main/java/org/example/model/entity/TodoEntity.java
@@ -2,16 +2,30 @@ package org.example.model.entity;
 
 import jakarta.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "todo")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class TodoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "title")
     private String title;
+    @Column(name = "description")
     private String description;
 
-    @ManyToOne
-    @JoinColumn(name = "board", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
     private BoardEntity board;
+
 }
