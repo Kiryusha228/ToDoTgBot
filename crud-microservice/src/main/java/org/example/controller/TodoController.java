0c16ebad35842d537f3296e019a20ad58a33e681
Kiryusha228 <125873893+Kiryusha228@users.noreply.github.com>
2024-09-18 17:28:08 +0300

    added Todo controller

---
+++ crud-microservice/src/main/java/org/example/controller/TodoController.java
@@ -0,0 +1,52 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.model.dto.BoardDto;
+import org.example.model.dto.TodoDto;
+import org.example.model.dto.UpdateBoardTitleDto;
+import org.example.model.dto.UpdateTodoDto;
+import org.example.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("todo")
+public class TodoController {
+    @Autowired
+    private final TodoService todoService;
+
+    @PostMapping("/add")
+    public ResponseEntity addTodo(@RequestBody TodoDto todoDto) {
+        todoService.addTodo(todoDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public List<TodoDto> getTodosByBoardId(Long boardId) {
+        return todoService.getTodosByBoardId(boardId);
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity updateTodoInfo(@RequestBody UpdateTodoDto updateTodoDto) {
+        todoService.updateTodoInfo(updateTodoDto.getId(), updateTodoDto.getTitle(),
+                updateTodoDto.getDescription());
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity deleteTodo(Long todoId) {
+        todoService.deleteTodo(todoId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/switch")
+    public ResponseEntity switchTodoDone(Long todoId){
+        todoService.switchTodoDone(todoId);
+        return ResponseEntity.ok().build();
+    }
+}
